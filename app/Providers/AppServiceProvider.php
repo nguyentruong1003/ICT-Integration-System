8a7e96f39ac99938a38b9e93d559eae52ddb43c3
Nguyễn Văn Trường <nguyenvantruong2k@gmail.com>
2023-05-31 21:05:49 +0700

    Test docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (env('APP_ENV') == 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
